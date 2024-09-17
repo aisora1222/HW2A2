220c895e9f0070355fa771ee24bc180c79bce5ce
Jewoo Lee <jewoo1222@gmail.com>
2024-09-17 01:15:10 -0400

    make imports clean

--- app/src/main/java/com/example/hw2a2/MainActivity.kt
+++ app/src/main/java/com/example/hw2a2/MainActivity.kt
@@ -3,21 +3,15 @@ package com.example.hw2a2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.hw2a2.ui.theme.HW2A2Theme
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
+import com.example.hw2a2.ui.theme.HW2A2Theme
 import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
